461c9648c942000a13eecf6ca95f4c50de53790c
Server Hermes <zhu@quant.local>
2026-08-07 01:25:57 +0800

    fix governance: quarantine re-review drops regime filter (restore historical samples)

--- research/factor_governance_lightgbm.py
+++ research/factor_governance_lightgbm.py
@@ -967,8 +967,7 @@ class FactorGovernanceLightGBMService:
                 JOIN trade_outcome_review r ON r.entry_decision_id = dfs.decision_id
                 JOIN factor_contribution_review f
                   ON f.review_id = r.review_id AND f.factor = dfs.factor
-                WHERE dl.regime_id IS NOT NULL AND dl.regime_id <> ''
-                  AND dfs.factor = ?
+                WHERE dfs.factor = ?
                 ORDER BY dl.decision_ts ASC, dfs.id ASC
                 LIMIT ?
                 """,
